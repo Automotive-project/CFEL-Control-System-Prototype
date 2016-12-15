c309cb4c8c92a00eb41c4e389d06bf3e860ffa59
Juang, Yi-Lin <frankyjuang@gmail.com>
2016-12-15 13:17:14 +0100

    Update camera options

--- test.py
+++ test.py
@@ -19,11 +19,16 @@ print dummy_motor.position
 ############
 # Ref1: http://lima.blissgarden.org/camera/pointgrey/doc/index.html
 # Ref2: http://lima.blissgarden.org/applications/tango/doc/index.html
-NUM_FRAMES = 1
-
 camera1 = PyTango.DeviceProxy("cfeld/limaccds/poingrey")
+
+NUM_FRAMES = 1
+MIN_EXPO_TIME = camera1.valid_ranges[0]
+MAX_EXPO_TIME = camera1.valid_ranges[1]
+EXPO_TIME = 0.1
+FILE_INDEX = 4
+
 camera1.acq_nb_frames = NUM_FRAMES
-camera1.acq_expo_time = 1.0
+camera1.acq_expo_time = EXPO_TIME
 camera1.prepareAcq()
 camera1.startAcq()
 # Wait until all images are captured.
@@ -31,12 +36,13 @@ while camera1.last_image_ready != NUM_FRAMES - 1:
     print camera1.last_image_ready + 1
     time.sleep(.1)
 # Read the first image in DATA_ARRAY format.
-image = camera1.readImage(0)
+# image = camera1.readImage(0)
 # Write the first image to file in RAW format.
 camera1.saving_directory = "/home/ax01user/test/"
 camera1.saving_prefix = "PG"
+camera1.saving_next_number = FILE_INDEX
 camera1.saving_suffix = "raw"
-# TODO: try other formats (edf, edfgz, cbf)
+# Todo: try other formats (edf, edfgz, cbf)
 camera1.saving_format = "RAW"
 camera1.saving_overwrite_policy = "OVERWRITE"
 camera1.writeImage(0)
